830a45259fbea1ac320e8a1bf874ae39b4ca2cbb
Sam Collins <sam@scollins.co>
2020-05-20 10:55:20 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,11 @@
 #!/bin/sh -l
 
+if [ -n "${IMAGE_TAG}" ]; then 
+    echo "IMAGE_TAG: ${IMAGE_TAG}"
+    IMAGE_TAG=$(echo ${IMAGE_TAG} | sed -e "s/refs\/heads\///g")
+    IMAGE_TAG=$(echo ${IMAGE_TAG} | sed -e "s/refs\/tags\///g" | sed -e "s/\//-/g") 
+    echo "IMAGE_TAG: ${IMAGE_TAG}"
+    SKAFFOLD_TAG=${IMAGE_TAG}
+fi 
+
 sh -c "skaffold ${INPUT_SKAFFOLD}"
